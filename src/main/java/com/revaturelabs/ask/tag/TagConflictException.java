0fd8683061f4d35d22b7c95466ddfb555dbb8982
dblitz <blitzy2k@gmail.com>
2019-11-06 10:58:37 -0500

    Added more documentations for Tag Exceptions

--- src/main/java/com/revaturelabs/ask/tag/TagConflictException.java
+++ src/main/java/com/revaturelabs/ask/tag/TagConflictException.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.CONFLICT)
 public class TagConflictException extends RuntimeException {
   /**
-   * 
+   * This exception is intended to be thrown during a Data integrity exception
    */
   private static final long serialVersionUID = 8418321298255656328L;
 
--- src/main/java/com/revaturelabs/ask/tag/TagNotFoundException.java
+++ src/main/java/com/revaturelabs/ask/tag/TagNotFoundException.java
@@ -3,6 +3,11 @@ package com.revaturelabs.ask.tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * 
+ * This exception will be thrown when a tag is not found in a database
+ *
+ */
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class TagNotFoundException extends RuntimeException {
 
